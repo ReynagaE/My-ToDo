3960eedeff6667733530e100b2f59c2d75afb6e9
ReynagaE <123265741+ReynagaE@users.noreply.github.com>
2023-07-17 13:32:54 -0600

    Credentials included

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,10 @@ import axios from "axios";
 const instance = axios.create({
     baseURL: 'https://taskminderapi.onrender.com/api',
     withCredentials: true,
-    credentials: 'include'
+    credentials: 'include',
+    headers: {
+        "Content-type": "application/json",
+    },
 })
 
 export default instance
