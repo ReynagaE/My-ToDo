392e9ef1699a0989a879d1826738b94c76f2cb03
ReynagaE <123265741+ReynagaE@users.noreply.github.com>
2023-07-19 15:58:53 -0600

    Hash Router

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -19,21 +19,25 @@ export const AuthProvider = ({children}) => {
     const [loading, setLoading] = useState(true);
 
     const signup = async (user) => {
+        const cookies = Cookies.get();
         try {
             const res = await registerRequest(user);
             console.log(res.data);
             setUser(res.data);
             setIsAuthenticated(true);
+            Cookies.set('token', cookies)
         } catch (error) {
             setErrors(error.response.data)
         }
     }
 
     const signin = async (user) => {
+        const cookies = Cookies.get();
         try {
             const res = await loginRequest(user)
             setUser(res.data);
             setIsAuthenticated(true);
+            Cookies.set('token', cookies)
         } catch (error) {
             if (Array.isArray(error.response.data)) {
                 return setErrors(error.response.data)
@@ -74,7 +78,7 @@ export const AuthProvider = ({children}) => {
             setIsAuthenticated(true);
             setUser(res.data);
             setLoading(false);
-            Cookies.set('token', cookies)
+            
           } catch (error) {
             setIsAuthenticated(false);
             setLoading(false);
