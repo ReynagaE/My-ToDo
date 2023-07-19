6325fef25b15f6b29cff0acfe7185a33fe769629
ReynagaE <123265741+ReynagaE@users.noreply.github.com>
2023-07-19 12:36:08 -0600

    No credentials

--- src/App.jsx
+++ src/App.jsx
@@ -21,12 +21,12 @@ function App() {
           <Route path='/login' element={<LoginPage/>} />
           <Route path='/register' element={<RegisterPage />} />
 
-          <Route element={<ProtectedRoute/>}>
+          
             <Route path='/tasks' element={<TasksPage/>} />
             <Route path='/add-task' element={<TaskFormPage/>} />
             <Route path='/tasks/:id' element={<TaskFormPage/>} />
             <Route path='/profile' element={<ProfilePage/>} />
-          </Route>
+          
         </Routes>
       </BrowserRouter>
       </ListProvider>
