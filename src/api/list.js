24de67640e1fb023cde5c22af8bf955e596ac617
ReynagaE <123265741+ReynagaE@users.noreply.github.com>
2023-07-18 19:46:48 -0600

    axios

--- src/api/list.js
+++ src/api/list.js
@@ -1,11 +1,11 @@
 import instance from "./axios";
 
-export const getListsRequest = () => instance.get("/lists");
+export const getListsRequest = () => instance.get('/lists')
 
-export const createListRequest = (list) => instance.post("/lists", list);
+export const createListsRequest = (list) => instance.post('/lists', list)
 
-export const updateListRequest = (list) => instance.put(`/lists/${list._id}`, list);
+export const updateListsRequest = (list) => instance.put(`/lists/${list._id}`, list)
 
-export const deleteListRequest = (id) => instance.delete(`/lists/${id}`);
+export const deleteListsRequest = (id) => instance.delete(`/lists/${id}`)
 
-export const getListRequest = (id) => instance.get(`/lists/${id}`);
+export const getListRequest = (id) => instance.get(`/lists/${id}`)
