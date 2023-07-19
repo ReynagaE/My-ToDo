fddf013b7b39d7abe2c12565b3e25df54e13d926
ReynagaE <123265741+ReynagaE@users.noreply.github.com>
2023-07-19 11:08:25 -0600

    No credentials

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,10 +2,15 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: 'https://taskminderapi.onrender.com/api',
-  // Agregar aquí el token en el encabezado de cada solicitud
-  headers: {
-    Authorization: localStorage.getItem("token") || "", // Asegúrate de tener el token almacenado en localStorage
-  },
+});
+
+// Agregar un interceptor para incluir el token en el encabezado "Authorization" de cada solicitud
+instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers["Authorization"] = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default instance;
